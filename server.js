39d442dca0c51315b0abe040a5d5876a2f5c25d4
hihoriyadarshan <112104707+hihoriyadarshan@users.noreply.github.com>
2023-08-21 23:59:53 +0530

    user  model,controller and route created

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import colors from 'colors';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
+import authRoutes from './routes/authRoute.js';
 
 //config env
 dotenv.config();
@@ -17,6 +18,8 @@ const app = express();
 app.use(express.json())
 app.use(morgan('dev'))
 
+//routes
+app.use('/api/v1/auth', authRoutes);
 
 //rest api
 app.get('/',(req,res) => {
