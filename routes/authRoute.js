4ba29dfcdb6a682239d646582c543efe032d05a2
hihoriyadarshan <112104707+hihoriyadarshan@users.noreply.github.com>
2023-08-24 12:24:28 +0530

    admin authentication successfully

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import {registerController,loginController} from '../controllers/authController.js'
+import {registerController,loginController,testController} from '../controllers/authController.js'
+import { requireSignIn,isAdmin } from '../middleware/authMiddleware.js';
 
 //router object
 const router =express.Router()
@@ -12,4 +13,6 @@ router.post("/register", registerController);
 //LOGIN || POST
 router.post("/login", loginController);
 
+//test
+router.get("/test",requireSignIn,isAdmin,testController)
 export default router
