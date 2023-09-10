7a07c8d2f5d6ae90b28a36542863a54d9654efa0
hihoriyadarshan <112104707+hihoriyadarshan@users.noreply.github.com>
2023-09-10 16:05:04 +0530

    category update and delete successfully

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -4,11 +4,15 @@ import AdminMenu from '../../components/Layout/AdminMenu'
 import toast from 'react-hot-toast';
 import axios from 'axios'
 import CategoryForm from '../../components/Form/CategoryForm';
+import { Modal } from "antd";
 
 
 const CreateCategory = () => {
   const [categories, setCategories] = useState([]);
   const [name, setName] = useState("");
+  const [visible, setVisible] = useState(false);
+  const [selected, setSelected] = useState(null);
+  const [updatedName, setUpdatedName] = useState("");
 
   //handle Form
   const handleSubmit = async (e) => {
@@ -29,7 +33,7 @@ const CreateCategory = () => {
     }
   };
 
-  //get all cat
+  //get all category
   const getAllCategory = async () => {
     try {
       const { data } = await axios.get(`${process.env.REACT_APP_API}/api/v1/category/get-category`);
@@ -45,6 +49,49 @@ const CreateCategory = () => {
   useEffect(() => {
     getAllCategory();
   }, []);
+
+
+//update category
+const handleUpdate = async (e) => {
+  e.preventDefault();
+  try {
+    const { data } = await axios.put(
+      `${process.env.REACT_APP_API}/api/v1/category/update-category/${selected._id}`,
+      { name: updatedName }
+    );
+    if (data?.success) {
+      toast.success(`${updatedName} is updated`);
+      setSelected(null);
+      setUpdatedName("");
+      setVisible(false);
+      getAllCategory();
+    } else {
+      toast.error(data.message);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+  //delete category
+  const handleDelete = async (pId) => {
+    try {
+      const { data } = await axios.delete(
+        `${process.env.REACT_APP_API}/api/v1/category/delete-category/${pId}`
+      );
+      if (data.success) {
+        toast.success(`category is deleted`);
+
+        getAllCategory();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error("Somtihing went wrong");
+    }
+  };
+
+
 
   return (
     <Layout>
@@ -72,7 +119,15 @@ const CreateCategory = () => {
                     <tr>
                       <td key={c._id}>{c.name}</td>
                       <td>
-                        <button className='btn btn-primary'>Edit</button>
+                        <button className='btn btn-primary ms-2'onClick={() => {setVisible(true); setUpdatedName(c.name); setSelected(c)} } >Edit</button>
+                        <button
+                            className="btn btn-danger ms-2"
+                            onClick={() => {
+                              handleDelete(c._id);
+                            }}
+                          >
+                            Delete
+                          </button>
                       </td>
                     </tr>
                     </>
@@ -83,6 +138,17 @@ const CreateCategory = () => {
 
               </table>
               </div>
+              <Modal
+              onCancel={() => setVisible(false)}
+              footer={null}
+              visible={visible}
+            >
+              <CategoryForm
+                value={updatedName}
+                setValue={setUpdatedName}
+                handleSubmit={handleUpdate}
+              />
+            </Modal>
             </div>
             </div>
           </div>
