cef6bb62f5d2a1c6cf2bf21c75ca1908ec1c46d4
hihoriyadarshan <112104707+hihoriyadarshan@users.noreply.github.com>
2023-09-17 18:52:56 +0530

    product show home page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,13 +1,69 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import Layout from '../components/Layout/Layout'
 import { useAuth } from '../context/auth'
+import { CgMouse } from "react-icons/cg";
+import axios from "axios";
+import "./css/Home.css"
 
 const Homepage = () => {
- const [auth,setAuth]=useAuth()
+ const [auth,setAuth]=useAuth();
+ const [products,setProducts] = useState([])
+ const [categories,setCategories] = useState([])
+
+ //get products
+ const getAllProducts = async () => {
+  try {
+    const { data } = await axios.get(`${process.env.REACT_APP_API}/api/v1/product/get-product`);
+    setProducts(data.products);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+useEffect(() => {
+  getAllProducts();
+}, []);
   return (
-    <Layout title={"best offer"}>
-      <h1>HomePage</h1>
-      <pre>{JSON.stringify(auth, null, 4)}</pre>
+    <Layout title="MultiMart">
+       <div className="banner">
+        <p>Welcome To Multi-Mart</p>
+        <h1>Find Amazing Products Below</h1>
+
+        <a href="#container">
+          <button>
+            Scroll <CgMouse />
+          </button>
+        </a>
+      </div>
+
+     <h2 className="homeHeading">Featured Products</h2>
+     <div className='row mt-3'>
+     <div className='col-md-3'>
+      <h4 className='text-center'>Filter By Category</h4>
+     </div>
+     <div className='col-md-9'>
+     <div className='d-flex flex-wrap'>
+     {products?.map((p) => (
+              
+                <div className="card m-2" style={{ width: "18rem" }}>
+                  <img
+                    src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${p._id}`}
+                    className="card-img-top"
+                    alt={p.name}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{p.name}</h5>
+                    <p className="card-text">{p.description}</p>
+                    <button className='btn btn-primary ms-1'>More Details</button>
+                    <button className='btn btn-Secondary ms-1'>ADD TO Cart</button>
+
+                  </div>
+                </div>
+             
+            ))}
+       </div>
+       </div>
+       </div>
     </Layout>
   )
 }
