d5cb2a4bee9254714e186b1b26726b88b790305f
hihoriyadarshan <112104707+hihoriyadarshan@users.noreply.github.com>
2023-10-01 20:29:50 +0530

    order pdf download

--- client/src/pages/Admin/AdminOrders.js
+++ client/src/pages/Admin/AdminOrders.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import toast from "react-hot-toast";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
 import AdminMenu from "../../components/Layout/AdminMenu";
 import Layout from "../../components/Layout/Layout";
-import { useAuth } from "../../context/auth";
 import moment from "moment";
-import { Select } from "antd";
+import { Select, Input, Button } from "antd";
+
 const { Option } = Select;
 
 const AdminOrders = () => {
@@ -13,137 +14,182 @@ const AdminOrders = () => {
     "Not Process",
     "Processing",
     "Shipped",
-    "deliverd",
+    "delivered",
     "cancel",
   ]);
   const [changeStatus, setChangeStatus] = useState("");
   const [orders, setOrders] = useState([]);
-  const [auth, setAuth] = useAuth();
-  const [currentPage, setCurrentPage] = useState(1); 
-  const [itemsPerPage] = useState(5); 
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(5);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredOrders, setFilteredOrders] = useState([]);
 
   const getOrders = async () => {
     try {
-      const { data } = await axios.get(`${process.env.REACT_APP_API}/api/v1/auth/all-orders`);
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API}/api/v1/auth/all-orders`
+      );
       setOrders(data);
+      setFilteredOrders(data);
     } catch (error) {
       console.log(error);
     }
   };
 
   useEffect(() => {
-    if (auth?.token) getOrders();
-  }, [auth?.token]);
+    getOrders();
+  }, []);
 
   const handleChange = async (orderId, value) => {
     try {
-      const { data } = await axios.put(`${process.env.REACT_APP_API}/api/v1/auth/order-status/${orderId}`, {
-        status: value,
-      });
+      const { data } = await axios.put(
+        `${process.env.REACT_APP_API}/api/v1/auth/order-status/${orderId}`,
+        {
+          status: value,
+        }
+      );
       getOrders();
     } catch (error) {
       console.log(error);
     }
   };
 
-  // Calculate index of the last item to be displayed on the current page
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+    filterOrders(e.target.value);
+  };
+
+  const filterOrders = (query) => {
+    const filtered = orders.filter((order) =>
+      order.buyer?.name.toLowerCase().includes(query.toLowerCase())
+    );
+    setFilteredOrders(filtered);
+    setCurrentPage(1);
+  };
+
   const indexOfLastItem = currentPage * itemsPerPage;
-  // Calculate index of the first item to be displayed on the current page
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  // Slice the orders array to display only the items for the current page
-  const currentOrders = orders.slice(indexOfFirstItem, indexOfLastItem);
+  const currentOrders = filteredOrders.slice(
+    indexOfFirstItem,
+    indexOfLastItem
+  );
 
   const paginate = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
 
+  // Function to generate and download a PDF
+  const generatePDF = () => {
+    const doc = new jsPDF();
+
+    doc.autoTable({
+      head: [["#", "Status", "Buyer", "Date", "Payment", "Quantity"]],
+      body: currentOrders.map((o, i) => [
+        i + 1,
+        o?.status,
+        o.buyer?.name,
+        moment(o.createAt).fromNow(),
+        o.payment.success ? "Success" : "Failed",
+        o.products.length,
+      ]),
+    });
+
+    doc.save("orders.pdf");
+  };
+
   return (
     <Layout title={"All Orders Data"}>
       <div className="container-fluid m-3 p-3">
-       <div className="row">
+        <div className="row">
           <div className="col-md-3">
             <AdminMenu />
           </div>
-
           <section className="panel important">
-          <div className="add">
-            <div className="head-2">
-            <div className="write-title"> Manage Orders
-                   </div>
-               </div>
-          </div>
+            <div className="add">
+              <div className="head-2">
+                <div className="write-title"> Manage Orders </div>
+              </div>
+            </div>
           </section>
-
-            <div className='panel important'>
-              {currentOrders.map((o, i) => {
-                return (
-                  <div className="border shadow" key={i}>
-                    <table className="table">
-                      <thead>
-                        <tr>
-                          <th scope="col">#</th>
-                          <th scope="col">Status</th>
-                          <th scope="col">Buyer</th>
-                          <th scope="col"> date</th>
-                          <th scope="col">Payment</th>
-                          <th scope="col">Quantity</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <td>{i + 1}</td>
-                          <td>
-                            <Select
-                              bordered={false}
-                              onChange={(value) => handleChange(o._id, value)}
-                              defaultValue={o?.status}
-                            >
-                              {status.map((s, i) => (
-                                <Option key={i} value={s}>
-                                  {s}
-                                </Option>
-                              ))}
-                            </Select>
-                          </td>
-                          <td>{o?.buyer?.name}</td>
-                          <td>{moment(o?.createAt).fromNow()}</td>
-                          <td>{o?.payment.success ? "Success" : "Failed"}</td>
-                          <td>{o?.products?.length}</td>
-                        </tr>
-                      </tbody>
-                    </table>
-                    <div className="container">
-                      {o?.products?.map((p, i) => (
-                        <div className="row mb-2 p-3 card flex-row" key={p._id}>
-                          <div className="col-md-4">
-                            <img
-                              src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${p._id}`}
-                              className="card-img-top"
-                              alt={p.name}
-                              width="100px"
-                              height={"100px"}
-                            />
-                          </div>
-                          <div className="col-md-8">
-                            <p>{p.name}</p>
-                            <p>{p.description.substring(0, 30)}</p>
-                            <p>Price : {p.price}</p>
-                          </div>
+          <div className="panel important">
+            <Button onClick={generatePDF} className="download-pdf">
+              Download PDF
+            </Button>
+            <Input
+              placeholder="Search by buyer name"
+              onChange={handleSearchChange}
+              value={searchQuery}
+            />
+            {currentOrders.map((o, i) => {
+              return (
+                <div className="border shadow" key={i}>
+                  <table className="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Buyer</th>
+                        <th scope="col"> date</th>
+                        <th scope="col">Payment</th>
+                        <th scope="col">Quantity</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>{i + 1}</td>
+                        <td>
+                          <Select
+                            bordered={false}
+                            onChange={(value) => handleChange(o._id, value)}
+                            defaultValue={o?.status}
+                          >
+                            {status.map((s, i) => (
+                              <Option key={i} value={s}>
+                                {s}
+                              </Option>
+                            ))}
+                          </Select>
+                        </td>
+                        <td>{o.buyer?.name}</td>
+                        <td>{moment(o.createAt).fromNow()}</td>
+                        <td>{o.payment.success ? "Success" : "Failed"}</td>
+                        <td>{o.products.length}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <div className="container">
+                    {o.products.map((p, i) => (
+                      <div className="row mb-2 p-3 card flex-row" key={p._id}>
+                        <div className="col-md-4">
+                          <img
+                            src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${p._id}`}
+                            className="card-img-top"
+                            alt={p.name}
+                            width="100px"
+                            height={"100px"}
+                          />
                         </div>
-                      ))}
-                    </div>
-                   </div>
-                );
-              })}
-              {/* Pagination */}
-          <div className="pagination">
+                        <div className="col-md-8">
+                          <p>{p.name}</p>
+                          <p>{p.description.substring(0, 30)}</p>
+                          <p>Price: {p.price}</p>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              );
+            })}
+            <div className="pagination">
               <ul className="pagination">
-                {Array(Math.ceil(orders.length / itemsPerPage))
+                {Array(Math.ceil(filteredOrders.length / itemsPerPage))
                   .fill()
                   .map((_, i) => (
                     <li
                       key={i}
-                      className={`page-item ${currentPage === i + 1 ? 'active' : ''}`}
+                      className={`page-item ${
+                        currentPage === i + 1 ? "active" : ""
+                      }`}
                     >
                       <button
                         className="page-link"
@@ -154,8 +200,8 @@ const AdminOrders = () => {
                     </li>
                   ))}
               </ul>
-              </div>
             </div>
+          </div>
         </div>
       </div>
     </Layout>
