7e61d6d9fd8e051cca72a614ad87ddaa2766cbf5
hihoriyadarshan <112104707+hihoriyadarshan@users.noreply.github.com>
2023-10-16 22:03:19 +0530

    .

--- client/src/pages/Admin/AdminDashboard.js
+++ client/src/pages/Admin/AdminDashboard.js
@@ -9,19 +9,19 @@ import { Link } from "react-router-dom";
 const AdminDashboard = () => {
   const [productCount, setProductCount] = useState(0);
   const [userCount, setUserCount] = useState(0);
-  const [ContactCount, setContactCount] = useState(0);
+  const [contactCount, setContactCount] = useState(0);
   const [feedbackCount, setFeedbackCount] = useState(0);
   const [categoryCount, setCategoryCount] = useState(0);
-  const [sub_categoryCount, setsub_CategoryCount] = useState(0);
-  const [orderCount, setorderCount] = useState(0);
+  const [subCategoryCount, setSubCategoryCount] = useState(0);
+  const [orderCount, setOrderCount] = useState(0);
 
-  //get product count
-  const getProductCount = async () => {
+  // Function to get the count of different entities
+  const getCount = async (endpoint, stateUpdater) => {
     try {
       const response = await axios.get(
-        `${process.env.REACT_APP_API}/api/v1/product/product-count`
+        `${process.env.REACT_APP_API}${endpoint}`
       );
-      setProductCount(response.data.total);
+      stateUpdater(response.data.total);
     } catch (error) {
       console.error(error);
       toast.error("Something Went Wrong");
@@ -29,112 +29,14 @@ const AdminDashboard = () => {
   };
 
   useEffect(() => {
-    getProductCount();
-  }, []);
-
-  //get user count
-  const getUserCount = async () => {
-    try {
-      const response = await axios.get(
-        `${process.env.REACT_APP_API}/api/v1/auth/user-count`
-      );
-      setUserCount(response.data.total);
-    } catch (error) {
-      console.error(error);
-      toast.error("Something Went Wrong");
-    }
-  };
-
-  useEffect(() => {
-    getUserCount();
-  }, []);
-
-  //contact count
-  const getContactCount = async () => {
-    try {
-      const response = await axios.get(
-        `${process.env.REACT_APP_API}/api/v1/auth/contacts-count`
-      );
-      setContactCount(response.data.total);
-    } catch (error) {
-      console.error(error);
-      toast.error("Something Went Wrong");
-    }
-  };
-
-  useEffect(() => {
-    getContactCount();
-  }, []);
-
-  //contact count
-  const getfeedbackCount = async () => {
-    try {
-      const response = await axios.get(
-        `${process.env.REACT_APP_API}/api/v1/feedback/feedback-count`
-      );
-      setFeedbackCount(response.data.total);
-    } catch (error) {
-      console.error(error);
-      toast.error("Something Went Wrong");
-    }
-  };
-
-  useEffect(() => {
-    getfeedbackCount();
-  }, []);
-
-  //category-count
-
-  const getcategoryCount = async () => {
-    try {
-      const response = await axios.get(
-        `${process.env.REACT_APP_API}/api/v1/category/category-count`
-      );
-      setCategoryCount(response.data.total);
-    } catch (error) {
-      console.error(error);
-      toast.error("Something Went Wrong");
-    }
-  };
-
-  useEffect(() => {
-    getcategoryCount();
-  }, []);
-
-  //sub-category count
-
-  const getsub_categoryCount = async () => {
-    try {
-      const response = await axios.get(
-        `${process.env.REACT_APP_API}/api/v1/category/sub-category-count`
-      );
-      setsub_CategoryCount(response.data.total);
-    } catch (error) {
-      console.error(error);
-      toast.error("Something Went Wrong");
-    }
-  };
-
-  useEffect(() => {
-    getsub_categoryCount();
-  }, []);
-
-  //order count
-
-  const getorderCount = async () => {
-    try {
-      const response = await axios.get(
-        `${process.env.REACT_APP_API}/api/v1/product/order-count`
-      );
-      setorderCount(response.data.total);
-    } catch (error) {
-      console.error(error);
-      toast.error("Something Went Wrong");
-    }
-  };
-
-  useEffect(() => {
-    getorderCount();
+    // Fetch counts for different entities
+    getCount("/api/v1/product/product-count", setProductCount);
+    getCount("/api/v1/auth/user-count", setUserCount);
+    getCount("/api/v1/auth/contacts-count", setContactCount);
+    getCount("/api/v1/feedback/feedback-count", setFeedbackCount);
+    getCount("/api/v1/category/category-count", setCategoryCount);
+    getCount("/api/v1/category/sub-category-count", setSubCategoryCount);
+    getCount("/api/v1/product/order-count", setOrderCount);
   }, []);
 
   return (
@@ -150,7 +52,6 @@ const AdminDashboard = () => {
                 <div className="write-title12"> Dashboard </div>
               </div>
             </div>
-            {/* </div> */}
           </section>
         </div>
       </div>
@@ -160,19 +61,17 @@ const AdminDashboard = () => {
           <div className="card-21">
             <Link to="/dashboard/admin/users">
               <center>
-                {" "}
                 <h2>Users</h2>
               </center>
               <br />
               <br />
-              <p1 font-size="200px;">{userCount} </p1>
+              <p1>{userCount} </p1>
             </Link>
           </div>
 
           <div className="card-21">
             <Link to="/dashboard/admin/AllProduct">
               <center>
-                {" "}
                 <h2>Total Product</h2>
               </center>
               <br />
@@ -184,7 +83,6 @@ const AdminDashboard = () => {
           <div className="card-21">
             <Link to="/dashboard/admin/create-category">
               <center>
-                {" "}
                 <h2>Category</h2>
               </center>
               <br />
@@ -198,14 +96,13 @@ const AdminDashboard = () => {
       <div className="container-22">
         <div className="row-21">
           <div className="card-21">
-            <Link to="/dashboard/admin/CreateSub_categoty">
+            <Link to="/dashboard/admin/CreateSub_category">
               <center>
-                {" "}
                 <h2>Sub-Category</h2>
               </center>
               <br />
               <br />
-              <p1 font-size="200px;">{sub_categoryCount} </p1>
+              <p1>{subCategoryCount} </p1>
             </Link>
           </div>
 
@@ -227,7 +124,7 @@ const AdminDashboard = () => {
               </center>
               <br />
               <br />
-              <p1> {ContactCount} </p1>
+              <p1> {contactCount} </p1>
             </Link>
           </div>
         </div>
@@ -238,32 +135,29 @@ const AdminDashboard = () => {
           <div className="card-21">
             <Link to="/dashboard/admin/get_all_feedback">
               <center>
-                {" "}
-                <h2>Feedback</h2>{" "}
+                <h2>Feedback</h2>
               </center>
               <br />
               <br />
-              <p1 font-size="200px;">{feedbackCount} </p1>
+              <p1>{feedbackCount} </p1>
             </Link>
           </div>
 
           <div className="card-21">
-            <Link to="">
+            <Link to="#">
               <center>
-                {" "}
-                <h2>Total Product</h2>{" "}
+                <h2>Unused Entity</h2>
               </center>
               <br />
               <br />
-              <p1>0 </p1>
+              <p1> 0 </p1>
             </Link>
           </div>
 
           <div className="card-21">
-            <Link to="">
+            <Link to="#">
               <center>
-                {" "}
-                <h2>Category</h2>
+                <h2>Another Entity</h2>
               </center>
               <br />
               <br />
